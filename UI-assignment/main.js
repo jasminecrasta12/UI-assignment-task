c2d8f29759a9eb56b55c8fc90bc13f5676a6effd
jasminecrasta12 <98318004+jasminecrasta12@users.noreply.github.com>
2022-01-25 16:17:25 +0530

    Updated UI changes.

--- UI-assignment/main.js
+++ UI-assignment/main.js
@@ -1,30 +1,31 @@
 function menuResponsive() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
+    var menu = document.getElementById("menuNav");
+    if (menu.className === "topnav") {
+        menu.className += " responsive";
     } else {
-      x.className = "topnav";
+        menu.className = "topnav";
     }
-  }
+}
 
-  fetch('https://reqres.in/api/users/').then((data)=>{
-        return data.json();
-  }).then((userList) => {
-      console.log(userList.data[1].id);
+fetch('https://reqres.in/api/users/').then((data) => {
+    return data.json();
+}).then((userList) => {
+    //console.log(userList.data[1].id);
 
-      let userData = "";
-      userList.data.map((values) =>{
-            userData += ` <div class="user-card">
+    let userData = "";
+    userList.data.map((values) => {
+        userData += ` <div class="user-card">
             <i class="fa fa-chevron-down card-arrow"></i>
             <img src=${values.avatar} alt="img">
             <p class="user-name">${values.first_name} ${values.last_name}</p>
+            <button class="user-designation">Developer</button>
             <p class="user-id"><span class="heading">Employee ID:</span>  <span>EMPP123${values.id}</span></p>
             <p class="user-email"><span class="heading">Email:</span> <span>${values.email}</span></p>
-            <p class="user-tel"><span class="heading">Telephone:</span> <span>+444 444 4444</span></p>
+            <p class="user-tel"><span class="heading">Telephone:</span> <span>+1 444 444 1234</span></p>
             <p class="user-dep"><span class="heading">Department:</span> <span>Digital Com</span></p>
         </div>`
-      })
+    })
 
-      document.getElementById('user-cards').innerHTML = userData;
-      
-  })
+    document.getElementById('user-cards').innerHTML = userData;
+
+})
